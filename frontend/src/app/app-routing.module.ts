573416227ad7cdc183e97a981445644e39326a19
Mathsqrt2 <micho.bugajski@gmail.com>
2024-07-21 22:43:41 +0200

    app structure improvements

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './layout/login/login.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "admin", component: LoginComponent },
   { path: "login", component: LoginComponent },
+  { path: "not-found", component: NotFoundPageComponent },
+  { path: "**", redirectTo: 'not-found'},
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { ContentComponent } from './layout/content/content.component';
 import { LoginComponent } from './layout/login/login.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent,
     LoginComponent,
+    NotFoundPageComponent,
   ],
   imports: [
     BrowserModule,
