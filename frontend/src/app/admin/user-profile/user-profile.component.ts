717b6fc72fa478d29ae3c6fa114b302b90859622
Mathsqrt2 <micho.bugajski@gmail.com>
2024-09-06 02:39:43 +0200

    verify with email form

--- frontend/src/app/admin/user-profile/user-profile.component.ts
+++ frontend/src/app/admin/user-profile/user-profile.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User, UsersService } from '../../services/users.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
 
-  currentUser: User;
-  permissions: { key: string, value: string }[] = []
+  public permissionsForm: FormGroup;
+  public confirmEmailForm: FormGroup;
+  public confirmEmailWithCodeForm: FormGroup;
+  public changePasswordForm: FormGroup
+
+  public currentUser: User;
+  public permissions: { key: string, value: string }[] = []
 
   public showCurrentPassword: boolean = false;
   public showNewPassword: boolean = false;
@@ -18,6 +24,9 @@ export class UserProfileComponent {
   public currentPassword: string = null;
   public newPassword: string = null;
   public confirmNewPassword: string = null;
+
+  public hasBeenEmailSend: boolean = false;
+  public isEmailConfirmed: boolean = false;
 
   constructor(
     private readonly usersService: UsersService
@@ -30,9 +39,48 @@ export class UserProfileComponent {
         for (let key of keys) {
           this.permissions.push({ key, value: this.currentUser.permissions[key] });
         }
-
       }
     });
+  }
+
+  ngOnInit(): void {
+    const { canUpdate, canDelete, canManage, canCreate } = this.currentUser.permissions;
+    this.permissionsForm = new FormGroup({
+      canUpdate: new FormControl({ value: canUpdate, disabled: !canManage }, [Validators.required]),
+      canDelete: new FormControl({ value: canDelete, disabled: !canManage }, [Validators.required]),
+      canManage: new FormControl({ value: canManage, disabled: !canManage }, [Validators.required]),
+      canCreate: new FormControl({ value: canCreate, disabled: !canManage }, [Validators.required]),
+    });
+
+    this.confirmEmailForm = new FormGroup({
+      newEmail: new FormControl(null, [Validators.required])
+    });
+
+    this.changePasswordForm = new FormGroup({
+      currentPassword: new FormControl(null),
+      newPassword: new FormControl(null),
+      confirmPassword: new FormControl(null),
+    });
+  }
+
+  onPasswordChange = () => {
+
+  }
+
+  onPermissionsUpdate = () => {
+
+  }
+
+  onEmailConfirm = () => {
+
+  }
+
+  onSendVerificationCode = () => {
+    this.hasBeenEmailSend = true;
+    this.confirmEmailWithCodeForm = new FormGroup({
+      newEmail: new FormControl({ value: this.confirmEmailForm.value.newEmail, disabled: true }),
+      confirmationCode: new FormControl(null, [Validators.required, Validators.minLength(9)])
+    })
   }
 
   onToggleVisibility = (field: string) => {
